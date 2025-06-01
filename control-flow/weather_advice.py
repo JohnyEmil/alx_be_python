d6b7ce2b1e916752730746de590e7db7f42edd22
JohnyEmil <jonyjony7030@gmail.com>
2025-06-01 20:02:41 +0300

    adding control-flow directory with weather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+user_info = input("What's the weather like today? (sunny/rainy/cold):")
+
+if user_info == "sunny":
+  print("Wear a t-shirt and sunglasses.")
+elif user_info == "rainy":
+  print("Don't forget your umbrella and a raincoat.")
+elif user_info == "cold":
+  print("Make sure to wear a warm coat and a scarf.")
+else:
+  print("Sorry, I don't have recommendations for this weather.")
