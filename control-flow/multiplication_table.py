5b40bdbc9a48da0ec72365c3703bc14e700a2336
JohnyEmil <jonyjony7030@gmail.com>
2025-06-01 20:24:24 +0300

    adding multip_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = float(input("Enter a number to see its multiplication table:"))
 
-for i in range(10):
-  print(f"{int(number)} * {i+1} = {int(number * (i+1))}")
+for i in range(1,11):
+  print(f"{int(number)} * {i} = {int(number * (i))}")
