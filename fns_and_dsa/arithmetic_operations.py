604f23e72bb138983049d24fb188bbebca817840
JohnyEmil <jonyjony7030@gmail.com>
2025-06-08 19:36:29 +0300

    adding first task

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,20 @@
 def perform_operation(num1, num2, operation):
-  num1 = float(num1)
-  num2 = float(num2)
-  operation = str(operation)
-  match operation:
-    case "add":
-      return num1+num2
-    case "subtract":
-      return num1-num2
-    case 'multiply':
-      return num1*num2
-    case 'divide':
-      if num2 == 0 :
-        return "division by zero not allowed"
-      else:
-        return num1/num2
-    case _:
+    num1 = float(num1)
+    num2 = float(num2)
+    operation = str(operation)
+
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "division by zero not allowed"
+        else:
+            return num1 / num2
+    else:
         raise ValueError(f"Invalid operation: {operation}")
   
   
