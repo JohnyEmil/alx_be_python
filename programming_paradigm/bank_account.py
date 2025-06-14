7bfa25bbeb763e444a93ee7147b199588497ce67
JohnyEmil <jonyjony7030@gmail.com>
2025-06-14 22:20:15 +0300

    adding the 0 task

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+  def __init__(self, account_balance):
+    self.account_balance = account_balance
+  
+  def deposit(self,amount):
+    self.account_balance += amount
+    return True
+  def withdraw(self,amount):
+    if amount <= self.account_balance:
+      self.account_balance -= amount
+      return True
+    return False
+  
+  def display_balance(self):
+    return print(f"Current Balance: ${self.account_balance}")
+
+
+
